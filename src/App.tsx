1cd98ddc4596e9754865852b3912da3b6926e145
박병조 <k97115284@gmail.com>
2025-04-04 22:52:25 +0900

    feat: InputComponent 작업 및 LoginPage 초안 생성 - Input Component 공용 생성 - LoginPage Route 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 import UserPage from './pages/UserPage';
 import AdminPage from './pages/AdminPage';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './routes/ProtectRoute';
 
 function App() {
+  const isAuthenticated = false; // TODO: 실제 상태로 대체
+  const role = 'user';        // TODO: 실제 상태로 대체  
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<AdminPage />} />
-      <Route path="/user" element={<UserPage />} />
+      <Route path="/" element={
+        isAuthenticated ? <Home /> : <Navigate to="/login" replace />
+      } />
+
+      <Route path="/login" element={
+        isAuthenticated ? <Navigate to="/" replace /> : <LoginPage />
+      } />
+
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated} role={role} element={<AdminPage />} />
+        }
+      />
+      
+      <Route
+        path="/user"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated} role={role} element={<UserPage />} />
+        }
+      />
     </Routes>
   );
 }
